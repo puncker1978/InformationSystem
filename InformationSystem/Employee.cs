759c426579bec1ff28a2945f309199b33f9f5298
Puncker1978 <nvv197827@gmail.com>
2021-12-11 19:14:22 +0500

    Добавлен класс Employee с необходимыми полями. Добавлено статическое поле _id, которое показывает сколько всего сотрудников в организации. Дбавлена константа salary - ставка заработной платы труда на 1 проект.

--- InformationSystem/Employee.cs
+++ InformationSystem/Employee.cs
@@ -6,5 +6,115 @@ namespace InformationSystem
 {
     internal class Employee
     {
+        #region Константы
+        /// <summary>
+        /// Ставка заработной платы труда на 1 проект
+        /// </summary>
+        private int salary;
+        #endregion
+
+        #region Статические поля
+        private static int _id;
+        #endregion
+
+        #region Поля
+        /// <summary>
+        /// Уникальный идентификационный номер сотрудника
+        /// </summary>
+        private int id;
+
+        /// <summary>
+        /// Фамилия сотрудника
+        /// </summary>
+        private string secondName;
+
+        /// <summary>
+        /// Имя сотрудника
+        /// </summary>
+        private string firstName;
+
+        /// <summary>
+        /// Возраст сотрудника
+        /// </summary>
+        private int age;
+
+        /// <summary>
+        /// Номер отдела, к которому прикреплён сотрудник
+        /// </summary>
+        private int idDepartment;
+
+        /// <summary>
+        /// Количество проектов, закрепленных за сотрудником
+        /// </summary>
+        private int projects;
+
+        /// <summary>
+        /// Итоговая заработная плата. Расчитывается как произведение количества проектов на ставку заработной платы.
+        /// </summary>
+        private int total;
+        #endregion
+
+        #region Свойства
+        /// <summary>
+        /// Уникальный идентификационный номер сотрудника
+        /// </summary>
+        public int Id { get => id; set => id = value; }
+
+        /// <summary>
+        /// Фамилия сотрудника
+        /// </summary>
+        public string SecondName { get => secondName; set => secondName = value; }
+
+        /// <summary>
+        /// Имя сотрудника
+        /// </summary>
+        public string FirstName { get => firstName; set => firstName = value; }
+
+        /// <summary>
+        /// Возраст сотрудника
+        /// </summary>
+        public int Age { get => age; set => age = value; }
+
+        /// <summary>
+        /// Номер отдела, к которому прикреплён сотрудник
+        /// </summary>
+        public int IdDepartment { get => idDepartment; set => idDepartment = value; }
+
+        /// <summary>
+        /// Количество проектов, закрепленных за сотрудником
+        /// </summary>
+        public int Projects { get => projects; set => projects = value; }
+
+        /// <summary>
+        /// Итоговая заработная плата. Расчитывается как произведение количества проектов на ставку заработной платы.
+        /// </summary>
+        public int Total { get => total; set => total = value; }
+
+
+        #endregion
+
+        #region Конструкторы
+        /// <summary>
+        /// Конструктор для создания сотрудника. Конструктор инициализирует поля: secondName,
+        /// firstName, age, idDepartment и projects. Cтатическое поле _id, уваличенное на единицу, присваивается
+        ///  полю id. Поле total (итоговая заработная плата) расчитывается как произведение количества
+        ///  проектов сотрудника projects на ставку salary.
+        /// </summary>
+        /// <param name="secondName">Фамилия сотрудника</param>
+        /// <param name="firstName">Имя сотрудника</param>
+        /// <param name="age">Возраст сотрудника</param>
+        /// <param name="idDepartment">Номер отдела, к которому прикреплён сотрудник</param>
+        /// <param name="projects">Количество проектов, закрепленных за сотрудником</param>
+        internal Employee(string secondName, string firstName, int age, int idDepartment, int projects)
+        {
+            this.Id = ++_id;
+            this.SecondName = secondName;
+            this.FirstName = firstName;
+            this.Age = age;
+            this.IdDepartment = idDepartment;
+            this.Projects = projects;
+            this.Total = salary * this.Projects;
+        }
+        #endregion
     }
 }
