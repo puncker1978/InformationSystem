2b5bf7c04c8ce368bb3cf50cfd5b30b5e62195a0
Puncker1978 <nvv197827@gmail.com>
2021-12-11 16:56:52 +0500

    Добавил класс Organization.cs В этот класс довавил приватные поля: idDepartment и idEmployee и internal-свойства IdDepartment и IdEmployee

---
+++ InformationSystem/Organization.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InformationSystem
+{
+    internal class Organization
+    {
+        #region Поля
+        /// <summary>
+        /// Уникальный идентификационный номер отдела
+        /// </summary>
+        private int idDepartment;
+
+        /// <summary>
+        /// Уникальный идентификационный номер сотрудника
+        /// </summary>
+        private int idEmployee;
+        #endregion
+
+        #region Свойства
+        /// <summary>
+        /// Уникальный идентификационный номер отдела
+        /// </summary>
+        internal int IdDepartment { get => idDepartment; set => idDepartment = value; }
+
+        /// <summary>
+        /// Уникальный идентификационный номер сотрудника
+        /// </summary>
+        internal int IdEmployee { get => idEmployee; set => idEmployee = value; }
+        #endregion
+    }
+}
