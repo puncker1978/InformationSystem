298c6a36ab49ed7e93909ae31c8d44e21e63d648
puncker1978 <puncker1978@mail.ru>
2022-01-11 13:19:09 +0500

    Добавил сортировку списка сотрудников по трём полям

--- InformationSystem/SortClass.cs
+++ InformationSystem/SortClass.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace InformationSystem
 {
@@ -9,5 +8,21 @@ namespace InformationSystem
     /// </summary>
     internal partial class Organization
     {
+        #region Сортировки сотрудников
+        internal List<Employee> sortedEmployees()
+        {
+            List<Employee> sortedEmployees = new List<Employee>();
+            sortedEmployees = (List<Employee>)(from employee in Employees
+                              orderby employee.FirstName,
+                              employee.Age,
+                              employee.Projects
+                              select Employees);
+
+            return sortedEmployees;
+        }
+        #endregion
+
+        #region Сортировки отделов
+        #endregion
     }
 }
