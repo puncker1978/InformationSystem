fa0e2f53aa92104abd53ddece4936f16be4bde49
Puncker1978 <nvv197827@gmail.com>
2022-01-17 23:24:56 +0500

    1

--- InformationSystem/SortClass.cs
+++ InformationSystem/SortClass.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
+using System.Collections.Generic;
 using System.Linq;
 
 namespace InformationSystem
